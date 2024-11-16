a417cf239f3717a089a3243a287b2369b29ef5b2
knot126 <73200930+knot126@users.noreply.github.com>
2024-11-16 16:20:29 +0000

    cleanup

--- Shim/obj/local/arm64-v8a/objs/android_native_app_glue/android_native_app_glue.o.d
+++
@@ -1,5 +0,0 @@
-/home/dragon/Development/Knot/Hyperspace/Shim/obj/local/arm64-v8a/objs/android_native_app_glue/android_native_app_glue.o: \
-  /home/dragon/Apps/NDK/android-ndk-r18b/sources/android/native_app_glue/android_native_app_glue.c \
-  /home/dragon/Apps/NDK/android-ndk-r18b/sources/android/native_app_glue/android_native_app_glue.h
-
-/home/dragon/Apps/NDK/android-ndk-r18b/sources/android/native_app_glue/android_native_app_glue.h:
